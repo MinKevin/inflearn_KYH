c9c5a9feff169030643d12f043e9e26a5595d2a7
MinKevin <alswkdrb1@naver.com>
2023-10-14 18:01:23 +0900

    스프링 MVC 1편 Sec.6 학습 및 복습(완)

---
+++ 스프링 MVC 1편/springmvc for practice/src/main/java/hello/springmvc/basic/MappingController.java
@@ -0,0 +1,123 @@
+package hello.springmvc.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MappingController {
+    private static Logger log = LoggerFactory.getLogger(MappingController.class);
+
+    /**
+     * 기본 요청
+     * 둘 다 허용 /hello-basic, /hello-basic/
+     * HTTP 메서드 모두 허용 GET, HEAD, POST, PUT, PATCH, DELETE
+     */
+    @RequestMapping("/hello-basic")
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    /**
+     * method: 특정 HTTP 메서드 요청만 허용
+     * GET, HEAD, POST, PUT, PATCH, DELETE
+     */
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
+    public String mappingGetV1() {
+        log.info("mappingGetV1");
+        return "ok";
+    }
+
+    /**
+     * 편리한 축약 애노테이션
+     *
+     * @GetMapping
+     * @PostMapping
+     * @PutMapping
+     * @PatchMapping
+     * @DeleteMapping
+     */
+    @GetMapping(value = "/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mappingGetV2");
+        return "ok";
+    }
+
+    /**
+     * PathVariable 사용
+     * 변수명이 같으면 생략 가능
+     * @PathVariable("userId") String userId => @PathVariable String userId
+     */
+    @GetMapping(value = "/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+
+    /**
+     * PathVariable 다중 사용
+     */
+    @GetMapping(value = "mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+    /**
+     * 파라미터로 추가 매핑
+     * params="mode",
+     * params="!mode",
+     * params="mode=debug",
+     * params="mode!=debug",
+     * params={"mode=debug", "data=good"}
+     */
+    @GetMapping(value="/mapping-param", params = {"mode=debug", "data=good"})
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    /**
+     * 특정 헤더로 추가 매핑
+     * headers="mode",
+     * headers="!mode",
+     * headers="mode=debug",
+     * headers="mode!=debug"
+     */
+    @GetMapping(value = "/mapping-header", headers = "mode=debug")
+    public String mappingHeader() {
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+    /**
+     * Content-Type 헤더 기반 추가 매핑 (Media-Type)
+     * consumes="application/json"
+     * consumes="!application/json"
+     * consumes="application/*"
+     * consumes"*\/*
+     *
+     * 아래와 같이 "application/json"을 MediaType.APPLICATION_JSON_VALUE로 직관적으로 대체 가능
+     */
+//    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    @PostMapping(value = "/mapping-consume", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    /**
+     * Accept 헤더 기반 Media 타입
+     * produces="text/html"
+     * produces="!text/html"
+     * produces="text/*"
+     * produces="*\/*"
+     */
+    @PostMapping(value = "/mapping-produce", produces="text/html")
+    public String mappingProduces() {
+        log.info("mappingProduces");
+        return "ok";
+    }
+}
